b4c5026e5b77a3e85dd6e4cb1c277df76c507b38
Hubyp <105743041+Hubyp@users.noreply.github.com>
2023-03-12 17:37:08 +0200

    Update exemplu.py

--- exemplu.py
+++ exemplu.py
@@ -1,14 +1,41 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+# Defining the client
+client = commands.Bot(command_prefix='!')
 
-@bot.command()
+# Defining the command
+@client.command()
 async def hello(ctx):
     await ctx.send('Hello!')
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+# Defining a command with arguments
+@client.command()
+async def greet(ctx, name):
+    await ctx.send(f'Hello, {name}!')
 
-bot.run('your_bot_token_here')
+# Event listener for bot startup
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+
+# Event listener for member join
+@client.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name='general')
+    await channel.send(f'Welcome to the server, {member.mention}!')
+
+# Event listener for member leave
+@client.event
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.channels, name='general')
+    await channel.send(f'{member.name} has left the server.')
+
+# Command error handling
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        await ctx.send('Command not found.')
+
+# Run the bot with your token
+client.run('your-token-here')
