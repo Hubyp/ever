770826b82cb1b1db98790cf4b088206ead1b7518
Hubyp <105743041+Hubyp@users.noreply.github.com>
2023-03-12 17:32:07 +0200

    Update exemple.js

--- exemple.js
+++ exemple.js
@@ -1,1 +1,15 @@
-# ever
+const { Client, Intents } =require("discord.js");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!Hi') {
+        message.channel.send('Hi !');
+    }
+});
+
+client.login(process.env.TOKEN);
